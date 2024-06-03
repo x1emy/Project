95679fa4a949719116dce54aef67f85f79081107
Lyailya <meytopintheworld@gmail.com>
2024-06-04 03:48:21 +0500

    First changes

---
+++ x1emy/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+from django.conf import settings 
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('',views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('socials', views.socials,name='socials')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
